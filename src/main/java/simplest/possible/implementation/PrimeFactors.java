a3d54ed3eb4c9328ac39c92ad26b7d006e92d58b
Devendra Kushwah <devendrakushwah@Devendras-MacBook-Pro.local>
2021-01-04 19:14:04 +0530

    Solution added PrimeFactors.java

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,11 @@
+package simplest.possible.implementation;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactors {
+
+    public <T> List<T> of(int i) {
+        return Collections.emptyList();
+    }
+}
